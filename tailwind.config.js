01b2ef763988e9e9ffd2972214e42a7b474f00e4
Jojo <me@jojostewart.com>
2022-10-30 23:12:28 +1300

    Update description

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        // "hero-pattern": "url('/src/musicle-bg.png')",
-        // "footer-texture": "url('/src/footer-texture.png')",
-      },
-    },
-  },
   plugins: [],
 };
